e26a5db66d32e66573e156ef4cc230cbdeb88f09
mazenfteha <mazn34903@gmail.com>
2023-07-24 16:06:30 -0700

    add data in sync way

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const { sequelize } = require('./models')
+const express = require('express')
 
-async function main() {
+const { sequelize, User } = require('./models')
+
+const app = express()
+app.use(express.json())
+
+//routes
+app.post('/users', async(req,res)=> {
+    const { name, email, role } = req.body
+
+    try {
+        const user = await User.create({ name, email, role })
+
+        return res.json(user)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
+})
+
+app.listen({ port: 5000 },async ()=> {
+    console.log('Server running on port 5000')
     await sequelize.sync({ force: true })
-}
-
-main()
+    console.log('Database synced!')
+})
