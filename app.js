8500fa900540f6dbd07e791c1ce0bf838f423a8b
mazenfteha <mazn34903@gmail.com>
2023-07-24 18:50:33 -0700

    endPoints

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(express.json())
 
 //routes
+//create user
 app.post('/users', async(req,res)=> {
     const { name, email, role } = req.body
 
@@ -18,6 +19,34 @@ app.post('/users', async(req,res)=> {
         return res.status(500).json(error)
     }
 })
+
+//fetch all users
+app.get('/users', async (req,res) => {
+    try {
+        const user = await User.findAll()
+
+        return res.json(user)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({error : 'Something went wrong'})
+    }
+})
+
+//find one user
+app.get('/users/:uuid', async (req,res) => {
+    const  uuid  = req.params.uuid
+    try {
+        const user = await User.findOne({ 
+            where: { uuid }
+         })
+
+        return res.json(user)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({error : 'Something went wrong'})
+    }
+})
+
 
 app.listen({ port: 5000 },async ()=> {
     console.log('Server running on port 5000')
